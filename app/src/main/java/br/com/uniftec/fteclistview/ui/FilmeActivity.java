ae83264998b23d096d084d1039931688dbb1c1d2
Daniel Ribeiro <dsribero276@gmail.com>
2017-09-28 19:42:43 -0300

    Alterações gerais.

--- app/src/main/java/br/com/uniftec/fteclistview/ui/FilmeActivity.java
+++ app/src/main/java/br/com/uniftec/fteclistview/ui/FilmeActivity.java
@@ -1,9 +1,11 @@
 package br.com.uniftec.fteclistview.ui;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import br.com.uniftec.fteclistview.R;
@@ -15,7 +17,10 @@ public class FilmeActivity extends AppCompatActivity implements View.OnClickList
 
     private Button fecharButton;
     private Filme filme;
-    private TextView textView;
+    private TextView textViewTitulo;
+    private TextView textViewNota;
+    private TextView textViewResumo;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +33,26 @@ public class FilmeActivity extends AppCompatActivity implements View.OnClickList
 
         filme = (Filme) getIntent().getSerializableExtra(FILME_PARAMETER);
 
-        textView = (TextView) findViewById(R.id.filme_id);
+        textViewTitulo = (TextView) findViewById(R.id.titulo_id);
+        textViewTitulo.setText(filme.getTitulo());
 
-        textView.setText(filme.getTitulo());
+        textViewNota = (TextView) findViewById(R.id.nota_id);
+        textViewNota.setText(String.valueOf(filme.getNota()));
 
+        textViewResumo = (TextView) findViewById(R.id.resumo_id);
+        textViewResumo.setText(filme.getResumo());
 
+        imageView = (ImageView) findViewById(R.id.image_filme_id);
+        //imageView
+
+        String nomeImagem = filme.getImagem();
+
+        String uri = "@drawable/"+nomeImagem;  // where myresource (without the extension) is the file
+
+        int imageResource = getResources().getIdentifier(uri, null, getPackageName());
+
+        Drawable res = getResources().getDrawable(imageResource, null);
+        imageView.setImageDrawable(res);
     }
 
     @Override
